70312a9e6935dfd8215bd563b6a65cdd54c7231a
Fosdick1 <fosdickfulfillment50@gmail.com>
2026-03-03 16:42:46 -0500

    Update engineered.js

--- src/services/services/cartonize/engineered.js
+++ src/services/services/cartonize/engineered.js
@@ -1,1 +1,17 @@
+export function engineeredCarton(packed, pad, rounding = 0.5) {
+  // Perfect fit carton dimensions = packed dims rounded up
+  const roundUp = (x) => Math.ceil(x / rounding) * rounding;
 
+  const L = roundUp(packed.packedL);
+  const W = roundUp(packed.packedW);
+  const H = roundUp(packed.packedH);
+
+  return {
+    name: "Engineered (Perfect Fit) Carton",
+    size: `${L}x${W}x${H}`,
+    inner_l: L,
+    inner_w: W,
+    inner_h: H,
+    notes: `Rounded up to nearest ${rounding}\" with padding included (${pad}\").`,
+  };
+}
